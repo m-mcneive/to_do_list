b7e91dbf4c1cd6882f909b5e0c029c7ea1dd63ed
Matt McNeive <matt.mcneive@gmail.com>
2023-07-22 08:34:35 -0500

    Base add functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let mut list: Vec<String> = vec![];
+    loop {
+        println!("What would you like to do? \nAdd \nRemove \nCheck");
+
+        let mut input = String::new();
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        match input.trim().to_lowercase().as_str() {
+            "add" => add(&mut list),
+            "delete" => println!("Can't delete yet"),
+            "check" => println!("Can't check yet"),
+            _ => println!("Do nothing"),
+        };
+    }
+}
+
+fn add(list: &mut Vec<String>) {
+    let mut item = String::new();
+
+    std::io::stdin()
+        .read_line(&mut item)
+        .expect("Failed to read line");
+
+    list.push(item);
+
 }
