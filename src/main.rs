5f055b7df978a1066972fcb4b32d1e32ea55301a
Matt McNeive <matt.mcneive@gmail.com>
2023-07-31 22:59:27 -0500

    Updates to printing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut list: Vec<String> = vec![];
     loop {
-        println!("What would you like to do? \nAdd \nRemove \nList");
+        println!("What would you like to do? \n1. Add \n2. Remove \n3. List");
 
         let mut input = String::new();
 
@@ -11,7 +11,7 @@ fn main() {
 
         match input.trim().to_lowercase().as_str() {
             "add" => add(&mut list),
-            "delete" => delete(&mut list),
+            "remove" => delete(&mut list),
             "list" => printItems(&list),
             _ => println!("Do nothing"),
         };
@@ -20,26 +20,30 @@ fn main() {
 
 fn add(list: &mut Vec<String>) {
     let mut item = String::new();
-
+    println!("Type an item to add...");
     std::io::stdin()
         .read_line(&mut item)
         .expect("Failed to read line");
 
+    print!("{}[2J", 27 as char);
     list.push(item);
 
 }
 
 fn delete(list: &mut Vec<String>) {
     let mut item = String::new();
-
+    println!("Type an item to delete");
     std::io::stdin()
         .read_line(&mut item)
         .expect("Failed to read line");
 
+    print!("{}[2J", 27 as char);
     list.retain(|x| *x != item);
 }
 
 fn printItems(list: &Vec<String>) {
+    print!("{}[2J", 27 as char);
+    println!("Your To-Do List");
     let mut index = 1;
     for item in list{
         print!("{}. {}", index, item);
